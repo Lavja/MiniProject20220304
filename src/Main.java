353d1db224247af2608954c2ed7eb2a7bd6610e1
rHidden <davidbuncek@gmail.com>
2022-03-04 10:57:30 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
+
 public class Main {
-	public void main(String args[]) {
-		//broken bone, fever, open wound, aches, bleeding, gender, age
-		Patient patient1 = new Patient("Susan", "Jones", "p123456",true,false,true,true,true,false,15);
-		Patient patient2 = new Patient("Morgan", "Stones", "p123456",false,true,false,true,false,true,26);
-		Patient patient3 = new Patient("Aaron", "Meachum", "p123456",false,true,false,true,false,true,55);
-		Patient patient4 = new Patient("Meghan", "Tenner", "p123456",false,true,false,false,false,false,72);
-		Patient patient5 = new Patient("Rick", "Grimes", "p123456",false,true,false,false,false,true,4);
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
 	}
+
 }
