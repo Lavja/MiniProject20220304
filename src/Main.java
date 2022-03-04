cb02c93a557de0160d212dee6a872c57864e9273
rHidden <davidbuncek@gmail.com>
2022-03-04 11:45:41 +0100

    System print line
    System print line

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
-
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Queuing q = new Queuing();
+		//broken bone, fever, open wound, aches, bleeding, gender, age
+		Patient patient1 = new Patient("Susan", "Jones", "p123456",true,false,true,true,true,false,15);
+		Patient patient2 = new Patient("Morgan", "Stones", "p123456",false,true,false,true,false,true,26);
+		Patient patient3 = new Patient("Aaron", "Meachum", "p123456",false,true,false,true,false,true,55);
+		Patient patient4 = new Patient("Meghan", "Tenner", "p123456",false,true,false,false,false,false,72);
+		Patient patient5 = new Patient("Rick", "Grimes", "p123456",false,true,false,false,false,true,4);
+		
+		System.out.println(q.getScore(patient1));	
+		System.out.println(q.getScore(patient2));
+		System.out.println(q.getScore(patient3));
+		System.out.println(q.getScore(patient4));
+		System.out.println(q.getScore(patient5));
 	}
-
 }
--- src/Queuing.java
+++ src/Queuing.java
@@ -4,12 +4,12 @@ public class Queuing {
 		int score = 0;
 		if(patient.getAge()<5 || patient.getAge()>75) score+=100;
 		if(patient.getAge()<15 || patient.getAge()>60) score+=50;
-		if(patient.isHasBrokenBone) score+=100;
+		if(patient.isHasBrokenBone()) score+=100;
 		if(patient.isBleeding) score+=50;
 		if(patient.isAching) score+=50;
-		if(patient.isHasFever) score+=50;
+		if(patient.isHasFever()) score+=50;
 		if(!patient.isMale) score+=20;
-		if(patient.isHasOpenWound) score+=45;
+		if(patient.isHasOpenWound()) score+=45;
 		
 		return score;
 	}
